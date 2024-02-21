354aa9527421638fcd64ee367108900809dae656
Javed-Patel-wohlig <javed.patel@wohlig.com>
2024-02-21 16:36:38 +0530

    restructure unzip file

--- unzip.js
+++ unzip.js
@@ -1,34 +1,25 @@
-const fs = require('fs');
-const zlib = require('zlib');
-const path = require("path");
+const fs = require("fs");
+const zlib = require("zlib");
 
 function decompressGzipFile(gzipFilePath, outputFilePath) {
   return new Promise((resolve, reject) => {
-
     const readStream = fs.createReadStream(gzipFilePath);
-
     const writeStream = fs.createWriteStream(outputFilePath);
 
-
-    readStream.pipe(zlib.createGunzip())
+    readStream
+      .pipe(zlib.createGunzip())
       .pipe(writeStream)
-      .on('finish', () => {
+      .on("finish", () => {
         console.log(`File decompressed and saved to ${outputFilePath}`);
         resolve(outputFilePath);
       })
-      .on('error', (err) => {
-        console.error('Error decompressing file:', err);
+      .on("error", (err) => {
+        console.error("Error decompressing file:", err);
         reject(err);
       });
   });
 }
 
-
-const rootFolder = path.join(__dirname, "s3files");
-// const gzipFilePath = rootFolder + "/13_06_31.gz-objectiQJOqs7c";
-// const outputFilePath = rootFolder + "/file.txt"
-
 module.exports = {
-    decompressGzipFile
-}
-
+  decompressGzipFile,
+};
